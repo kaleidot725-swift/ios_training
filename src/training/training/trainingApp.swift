7bf58528c8c8f41675b19c3a58da9727bab93b62
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2021-04-06 09:47:33 +0900

    feat: add new project

---
+++ src/training/training/trainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  trainingApp.swift
+//  training
+//
+//  Created by kaleidot725 on 2021/04/06.
+//
+
+import SwiftUI
+
+@main
+struct trainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
